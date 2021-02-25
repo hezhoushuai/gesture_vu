03815f5c2e78702c931f17ea140af6eeebdcc6ec
hezhoushuai <993578319@qq.com>
2021-02-25 23:28:43 +0800

    添加basePath前缀

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
+const basePath = process.env.NODE_ENV === 'development' ? '/' : '/gesture_vu/'
+
 module.exports = {
+  publicPath: basePath,
+  outputDir: 'docs',
   devServer: {
     port: 8088
   }
